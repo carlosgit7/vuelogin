2396fddfe4e9c334c6b13d967c6a83dbb0f6b35f
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-03-03 16:36:00 -0600

    improved secret page

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,54 @@
 <template>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor est officia nobis facere provident fugiat ducimus modi aut, atque corporis velit ipsam a tenetur, praesentium voluptate quidem, quod deleniti numquam?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore corporis maxime facilis ipsa quasi commodi laboriosam autem sit incidunt, dicta quod odit. Modi sequi quas dolor quidem a porro quos?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur voluptate ducimus nobis deserunt? Necessitatibus voluptatem laborum quaerat. Temporibus, consequuntur deleniti ipsa veritatis ducimus iste repellat optio repudiandae, est praesentium esse.
+    <h1 class="text-6xl font-extralight">This is your private section</h1>
+    <h2 class="text-3xl font-light">Here is some encourament for your coding day</h2>
+    <img src="../assets/banner.jpeg" alt="Know your limitations, then defy them" class="object-scale-down h-56 mt-5 shadow-2xl shadow-black border-2">
+    <div class=" w-4/5 text-center px-5 mt-10 space-y-5 text-blue-900">
+
+        <p class="text-xl font-extralight">
+            “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” 
+            <span class="italic"> - Martin Fowler</span> 
+        </p> 
+        <p class="text-xl font-extralight">
+            “First, solve the problem. Then, write the code.” 
+            <span class="italic"> - John Johnson</span>
+        </p>
+        <p class="text-xl font-extralight">
+            “Experience is the name everyone gives to their mistakes.” 
+            <span class="italic"> - Oscar Wilde </span>
+        </p>
+        <p class="text-xl font-extralight">
+            “In order to be irreplaceable, one must always be different”
+            <span class="italic">  - Coco Chanel </span>
+        </p>
+        <p class="text-xl font-extralight">
+            “Java is to JavaScript what car is to Carpet.”
+            <span class="italic">  - Chris Heilmann </span>
+        </p>
+        <p class="text-xl font-extralight">
+            “Knowledge is power.”
+            <span class="italic">  - Francis Bacon </span>
+        </p>
+        <p class="text-xl font-extralight">
+            “Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday's code.”
+            <span class="italic">  - Dan Salomon </span>
+        </p>
+        <p class="text-xl font-extralight">
+            “Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away.” 
+            <span class="italic">  - Antoine de Saint-Exupery </span>
+        </p>
+        <p class="text-xl font-extralight">
+            “ Code is like humor. When you have to explain it, it's bad.” 
+            <span class="italic">  - Cory House </span>
+        </p>
+        <p class="text-xl font-extralight">
+                “Simplicity is the soul of efficiency.” 
+            <span class="italic">  - Austin Freeman</span>
+        </p>
+        <p class="text-xl font-normal">
+            Trust in the Lord with all your heart and lean not on your own understanding;
+            in all your ways submit to him,
+            and he will make your paths straight.
+            <span class="italic"> - Proverbs 3:6</span>
+        </p>
+   </div>
 </template>
