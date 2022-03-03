87d81954665a88ee8767fb3d42c11c5a69197ee7
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-03-02 19:06:48 -0600

    added h2 to homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,8 +4,11 @@ const { isAuthenticated, user } = useAuth();
 </script>
 
 <template>
-<div class="flex flex-col space-y-10 items-center justify-center min-h-screen-nonav">
-   <h1 v-if="!isAuthenticated" class="text-6xl tracking-tighter font-extralight">Welcome</h1>
+<div class="flex flex-col items-center justify-center min-h-screen-nonav">
+  <div v-if="!isAuthenticated" class="">
+     <h1  class="text-6xl tracking-tighter font-extralight">Welcome</h1>
+     <h2 class=" tracking-tighter font-extralight text-blue-700 ml-5">Please login or create accout</h2>
+  </div>
    <h1 v-else class="text-6xl tracking-tighter font-extralight">Welcome {{ user }}</h1>
   <img src="../assets/indexBG.png" alt="bg-image" class="w-2/3 ">
 </div>
